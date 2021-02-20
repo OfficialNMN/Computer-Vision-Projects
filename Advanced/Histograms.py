775a081798946ec7c0426e58683825b5ab24815a
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2021-02-20 17:49:06 +0530

    Update Histograms.py

--- Advanced/Histograms.py
+++ Advanced/Histograms.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=cv.imread(r'C:\Users\NAMANJEET SINGH\PycharmProjects\OpenCVProject\pics\cats.jpg')
+img=cv.imread('cats.jpg')
 cv.imshow('original',img)
 
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
